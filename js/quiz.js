abd0beca9632b1256e666acb7af900281577c7c1
Nicole George <hello@nicolegeorge.me>
2020-03-15 16:17:02 +1030

    showing one question at a time using the show() and hide() functions. Results is the final screen.

--- js/quiz.js
+++ js/quiz.js
@@ -1,5 +1,39 @@
 $(document).ready(function() {
-      alert(1);
+    // Hiding all questions//
+    $('.questionForm').hide();
+
+    // Show first question
+    $('#q1').show();
+
+    $('#q1 #submit').click(function(){
+        $('.questionForm').hide();
+        $('#q2').show();
+        return false;
+    })
+
+    $('#q2 #submit').click(function(){
+        $('.questionForm').hide();
+        $('#q3').show();
+        return false;
+    })
+
+    $('#q3 #submit').click(function(){
+        $('.questionForm').hide();
+        $('#q4').show();
+        return false;
+    })
+
+    $('#q4 #submit').click(function(){
+        $('.questionForm').hide();
+        $('#q5').show();
+        return false;
+    })
+
+    $('#q5 #submit').click(function(){
+        $('.questionForm').hide();
+        $('#results').show();
+        return false;
+    })
+
 });
 
-// testing connection - works! //
