2521c4a3fc3646282897e506ffc6c262b74d84cf
Nicole George <hello@nicolegeorge.me>
2020-03-15 17:51:53 +1030

    quiz is working, scores are incrementing and link to take quiz over connected

--- js/quiz.js
+++ js/quiz.js
@@ -8,9 +8,9 @@ var highest = total * point;
 function init() {
     //setting correct answers - using session storage API 
     // syncing up the answers to each question
-    sessionStorage.setItem('a2', 'c');
-    sessionStorage.setItem('a1', 'd'); 
-    sessionStorage.setItem('a3', 'd');
+    sessionStorage.setItem('a1', 'd');
+    sessionStorage.setItem('a2', 'c'); 
+    sessionStorage.setItem('a3', 'b');
     sessionStorage.setItem('a4', 'c');
     sessionStorage.setItem('a5', 'c');
 }
@@ -24,36 +24,80 @@ $(document).ready(function() {
 
     $('#q1 #submit').click(function(){
         $('.questionForm').hide();
+        process('q1');
         $('#q2').show();
         return false;
     })
 
     $('#q2 #submit').click(function(){
         $('.questionForm').hide();
+        process('q2');
         $('#q3').show();
         return false;
     })
 
     $('#q3 #submit').click(function(){
         $('.questionForm').hide();
+        process('q3');
         $('#q4').show();
         return false;
     })
 
     $('#q4 #submit').click(function(){
         $('.questionForm').hide();
+        process('q4');
         $('#q5').show();
         return false;
     })
 
     $('#q5 #submit').click(function(){
         $('.questionForm').hide();
+        process('q5');
         $('#results').show();
         return false;
     })
 
 });
 
-// Add an event listener to compare the submitted answers against the correct answers
+// Processing the answers: compare the submitted answers against the correct answers
+
+function process(q) {
+    if(q == 'q1') {
+        var submitted = $('input[name = q1]:checked').val();
+        if (submitted == sessionStorage.a1) {
+            score++; // incrementing the score if the correct answer is submitted
+        }
+    }
+    // repeating the process for all quetions
+    if(q == 'q2') {
+        var submitted = $('input[name = q2]:checked').val();
+        if (submitted == sessionStorage.a2) {
+            score++ ;
+        }
+    }
+    if(q == 'q3') {
+        var submitted = $('input[name = q3]:checked').val();
+        if (submitted == sessionStorage.a3) {
+            score++; 
+        }
+    }
+    if(q == 'q4') {
+        var submitted = $('input[name = q4]:checked').val();
+        if (submitted == sessionStorage.a4) {
+            score++;
+        }
+    }
+    if(q == 'q5') {
+        var submitted = $('input[name = q5]:checked').val();
+        if (submitted == sessionStorage.a5) {
+            score++; 
+        }
+        $('#results').html('<h3>Your Final Score is ' + score + 
+        ' out of 5.</h3><a href = "index.html">Take Quiz Again</a>')
+    }
+    return false;
+}
+
+// Add an event listener 
 
 window.addEventListener('load', init, false);
